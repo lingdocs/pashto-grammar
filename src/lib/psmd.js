560b335abcf9cf0a7dcab38b76c6db875fe5a078
lingdocs <71590811+lingdocs@users.noreply.github.com>
2021-06-24 13:29:41 +0400

    starting to use jsx/md for highlighting in ps text

---
+++ src/lib/psmd.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Markdown from "markdown-to-jsx";
+
+export default function mdps(input) {
+    if (Array.isArray(input)) {
+        return input.map((x) => mdps(x));
+    }
+    return {
+        ...input,
+        p: <Markdown>{input.p}</Markdown>,
+        f: <Markdown>{input.f}</Markdown>,
+        ...input.e ? {
+            e: <Markdown>{input.e}</Markdown>,
+        } : {},
+    };
+}
