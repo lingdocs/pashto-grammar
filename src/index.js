b1cbbe9e303277ed1099d3d27a8af190657747d9
lingdocs <71590811+lingdocs@users.noreply.github.com>
2021-06-25 11:51:16 +0400

    turn on sw for pwa

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 // serviceWorker.register({
 //   onUpdate: registration => {
 //     const ready = window.confirm("New version available! Update?");
