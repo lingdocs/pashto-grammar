ae1b83062a55da2e36fa6096e488ab8df14597a1
lingdocs <71590811+lingdocs@users.noreply.github.com>
2022-06-25 13:20:45 -0500

    more on mini-pronouns

--- src/games/sub-cores/EquativeGame.tsx
+++ src/games/sub-cores/EquativeGame.tsx
@@ -503,7 +503,7 @@ function makeEPS(subject: T.NPSelection, predicate: T.AdjectiveEntry | T.Locativ
 }
 
 function getEqFromRendered(e: T.EPRendered): T.EquativeRendered {
-    const eblock = e.blocks[0].find(x => x.type === "equative");
-    if (!eblock || eblock.type !== "equative") throw new Error("Error getting equative block");
-    return eblock.equative;
+    const eblock = e.blocks[0].find(x => x.block.type === "equative");
+    if (!eblock || eblock.block.type !== "equative") throw new Error("Error getting equative block");
+    return eblock.block.equative;
 }
