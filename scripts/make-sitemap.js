d104f0292b457bb96582cb3b9d19100012a9cfc2
adueck <adueck@users.noreply.github.com>
2023-12-25 22:56:43 +0400

    update sitemap

--- scripts/make-sitemap.js
+++ scripts/make-sitemap.js
@@ -7,32 +7,38 @@ console.log("generating sitemap.xml");
 const indexText = fs.readFileSync("./src/content/index.ts", "utf-8");
 const contentTreeRaw = indexText.split("/* content-tree */")[1];
 const safeContentTreeText = contentTreeRaw
-    .split("\n")
-    .filter(l => !l.includes(`"import":`))
-    .join("\n");
+  .split("\n")
+  .filter((l) => !l.includes(`"import":`))
+  .join("\n");
 const contentTree = JSON.parse(safeContentTreeText);
 
-const urls = contentTree.reduce((acc, section) => {
+const urls = contentTree.reduce(
+  (acc, section) => {
     if ("slug" in section) {
-        return [
-            ...acc,
-            `${base}/${section.slug}`,
-        ];
+      return [...acc, `${base}/${section.slug}/`];
     } else {
-        return [
-            ...acc,
-            ...section.chapters.map(x => `${base}/${section.subdirectory}/${x.slug}`),
-        ];
+      return [
+        ...acc,
+        ...section.chapters.map(
+          (x) => `${base}/${section.subdirectory}/${x.slug}/`
+        ),
+      ];
     }
-}, [base]);
+  },
+  [`${base}`]
+);
 
 const siteMap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(u => `   <url>
+${urls
+  .map(
+    (u) => `   <url>
         <loc>${u}</loc>
     </url>    
-`).join("")}
-</urlset>
 `
+  )
+  .join("")}
+</urlset>
+`;
 
 fs.writeFileSync("./public/sitemap.xml", siteMap);
