755d5129588730b62753ac404e82aa7e3ded5ce6
adueck <adueck@users.noreply.github.com>
2025-08-20 21:19:23 +0500

    trying netlify manual deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-// import { registerSW } from "virtual:pwa-register";
+import { registerSW } from "virtual:pwa-register";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./user-context";
 
-// const updateSW = registerSW({
-//   onNeedRefresh() {
-//     if (confirm("New content available. Reload?")) {
-//       updateSW(true);
-//     }
-//   },
-//   onOfflineReady() {
-//     console.log("offline ready");
-//   },
-// });
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("offline ready");
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import rehypeSlug from "rehype-slug";
 import rehypeToc from "@stefanprobst/rehype-extract-toc";
 import rehypeTocExport from "@stefanprobst/rehype-extract-toc/mdx";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 import remarkGfm from "remark-gfm";
 
 // https://vite.dev/config/
@@ -36,68 +36,68 @@ export default defineConfig({
         rehypeTocExport,
       ],
     }),
-    // VitePWA({
-    //   workbox: {
-    //     // globPatterns: ["**/*.(mp4|m4a)"],
-    //     globPatterns: ["*/*.*", "*.*"],
-    //     maximumFileSizeToCacheInBytes: 5242880,
-    //   },
-    //   includeAssets: ["**/*.(js|html|svg|png|jpg|jpeg|eot|woff|woff2|ttf)"],
-    //   manifest: {
-    //     short_name: "Pashto Grammar",
-    //     name: "LingDocs Pashto Grammar",
-    //     icons: [
-    //       {
-    //         src: "icons/android-chrome-36x36.png",
-    //         sizes: "36x36",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-48x48.png",
-    //         sizes: "48x48",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-72x72.png",
-    //         sizes: "72x72",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-96x96.png",
-    //         sizes: "96x96",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-144x144.png",
-    //         sizes: "144x144",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-256x256.png",
-    //         sizes: "256x256",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-384x384.png",
-    //         sizes: "384x384",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "icons/android-chrome-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     start_url: "./table-of-contents",
-    //     display: "minimal-ui",
-    //     theme_color: "#DEE1E6",
-    //     background_color: "#ffffff",
-    //   },
-    // }),
+    VitePWA({
+      workbox: {
+        // globPatterns: ["**/*.(mp4|m4a)"],
+        globPatterns: ["*/*.*", "*.*"],
+        maximumFileSizeToCacheInBytes: 5242880,
+      },
+      includeAssets: ["**/*.(js|html|svg|png|jpg|jpeg|eot|woff|woff2|ttf)"],
+      manifest: {
+        short_name: "Pashto Grammar",
+        name: "LingDocs Pashto Grammar",
+        icons: [
+          {
+            src: "icons/android-chrome-36x36.png",
+            sizes: "36x36",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "./table-of-contents",
+        display: "minimal-ui",
+        theme_color: "#DEE1E6",
+        background_color: "#ffffff",
+      },
+    }),
   ],
 });
